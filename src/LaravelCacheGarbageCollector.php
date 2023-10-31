c6af14b43786982d0a16a12c1fa7f37b14abc5b1
StivenKatuuk <stiven.katuuk@simajji.com>
2023-10-31 11:02:50 +0800

    build(core): laravel 10 support

--- src/LaravelCacheGarbageCollector.php
+++ src/LaravelCacheGarbageCollector.php
@@ -38,38 +38,37 @@ class LaravelCacheGarbageCollector extends Command
             'driver'=>'local',
             'root'=>config('cache.stores.file.path')
         ];
+
         Config::set('filesystems.disks.fcache', $cacheDisk);
+
         $expired_file_count = 0;
         $active_file_count = 0;
 
         // Grab the cache files
         $files = Storage::disk('fcache')->allFiles();
 
-        // Loop the files and get rid of any that have expired
         foreach ($files as $key=>$cachefile) {
-            // Ignore this file
             if ($cachefile == '.gitignore') {
                 continue;
             }
 
             try {
-                // Grab the contents of the file
                 $contents = Storage::disk('fcache')->get($cachefile);
 
                 // Get the expiration time
                 $expire = substr($contents, 0, 10);
 
-                // See if we have expired
                 if (Carbon::now()->timestamp >= $expire) {
-                    // Delete the file
                     Storage::disk('fcache')->delete($cachefile);
+
                     $expired_file_count++;
-                } else {
-                    $active_file_count++;
+
+                    continue;
                 }
+
+                $active_file_count++;
             } catch (FileNotFoundException $e) {
-                // Getting an occasional error of this type on the 'get' command above,
-                // so adding a try-catch to skip the file if we do.
+                continue;
             }
         }
     }
